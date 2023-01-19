3b873dde0dac3df92fba20c4d15618e6b8216421
rajeevriitm <rajeevriitm@gmail.com>
2023-01-20 02:17:49 +0530

    implemented points for drawing

--- doodle-wasm/src/lib.rs
+++ doodle-wasm/src/lib.rs
@@ -1,10 +1,19 @@
-use std::cell::Cell;
+use std::cell::{Cell, RefCell};
 use std::rc::Rc;
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
-
+#[derive(Debug, Default)]
+#[wasm_bindgen]
+struct Drawing {
+    points: Rc<RefCell<Vec<Vec<(i32, i32)>>>>,
+}
+#[wasm_bindgen]
+impl Drawing {
+    fn send_points_to_backend(&self) {}
+}
 #[wasm_bindgen(start)]
 pub fn start() -> Result<(), JsValue> {
+    let drawing = Drawing::default();
     let document = web_sys::window().unwrap().document().unwrap();
     let canvas = document
         .create_element("canvas")?
@@ -19,6 +28,7 @@ pub fn start() -> Result<(), JsValue> {
         .dyn_into::<web_sys::CanvasRenderingContext2d>()?;
     let context = Rc::new(context);
     let pressed = Rc::new(Cell::new(false));
+    let points_clone = drawing.points.clone();
     {
         let context = context.clone();
         let pressed = pressed.clone();
@@ -26,10 +36,15 @@ pub fn start() -> Result<(), JsValue> {
             context.begin_path();
             context.move_to(event.offset_x() as f64, event.offset_y() as f64);
             pressed.set(true);
+            points_clone
+                .borrow_mut()
+                .push(vec![(event.offset_x(), event.offset_y())]);
         });
         canvas.add_event_listener_with_callback("mousedown", closure.as_ref().unchecked_ref())?;
         closure.forget();
     }
+    let points_clone = drawing.points.clone();
+
     {
         let context = context.clone();
         let pressed = pressed.clone();
@@ -39,6 +54,11 @@ pub fn start() -> Result<(), JsValue> {
                 context.stroke();
                 context.begin_path();
                 context.move_to(event.offset_x() as f64, event.offset_y() as f64);
+                points_clone
+                    .borrow_mut()
+                    .last_mut()
+                    .unwrap()
+                    .push((event.offset_x(), event.offset_y()));
             }
         });
         canvas.add_event_listener_with_callback("mousemove", closure.as_ref().unchecked_ref())?;
